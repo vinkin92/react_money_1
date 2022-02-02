7b48f7bed0bcfb3d810922633a7922ff53d67131
vinkim <928287706@qq.com>
2022-02-02 18:27:34 +0800

    创建 Layout 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,41 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+import Layout from './components/Layout';
 
 
 function App() {
     return (
-        <Wrapper>
-            <Main>
-                <Routes>
-                    <Route index element={<Money/>}/>
-                    <Route path="tags" element={<Tags/>}/>
-                    <Route path="money" element={<Money/>}/>
-                    <Route path="statistics" element={<Statistics/>}/>
-                    <Route path="*" element={<Notfind/>}/>
-                </Routes>
-            </Main>
-            <Nav />
-        </Wrapper>
+        <Routes>
+            <Route index element={<Money/>}/>
+            <Route path="tags" element={<Tags/>}/>
+            <Route path="money" element={<Money/>}/>
+            <Route path="statistics" element={<Statistics/>}/>
+            <Route path="*" element={<Notfind/>}/>
+        </Routes>
     );
 }
 
 function Tags() {
     return (
-        <div>标签页</div>
+        <Layout>
+            <div>标签页</div>
+        </Layout>
     );
 }
 
 function Money() {
     return (
+    <Layout>
         <div>记账页</div>
+    </Layout>
     );
 }
 
 function Statistics() {
     return (
+    <Layout>
         <div>统计页</div>
+    </Layout>
     );
 }
 
