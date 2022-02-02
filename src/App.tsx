e1b40a4307ddfbc8ec5f2ef2debfcd9094c60503
vinkim <928287706@qq.com>
2022-02-02 14:25:35 +0800

    添加 react-router + 导航栏基本布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,83 @@
 import React from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  border: 1px solid blue;
+  flex-grow: 1;
+  overflow: auto;
+`;
+const Nav = styled.nav`
+  border: 1px solid pink;
+
+  > ul {
+    display: flex;
+
+    > li {
+      width: 33.3%;
+      text-align: center;
+    }
+  }
+`;
 
 function App() {
     return (
-        <div>
-            你好
-        </div>
+        <Wrapper>
+            <Main>
+                <Routes>
+                    <Route index element={<Money/>}/>
+                    <Route path="tags" element={<Tags/>}/>
+                    <Route path="money" element={<Money/>}/>
+                    <Route path="statistics" element={<Statistics/>}/>
+                    <Route path="*" element={<Notfind/>}/>
+                </Routes>
+            </Main>
+            <Nav>
+                <ul>
+                    <li>
+                        <Link to="/tags">标签页</Link>
+                    </li>
+                    <li>
+                        <Link to="/money">记账页</Link>
+                    </li>
+                    <li>
+                        <Link to="/statistics">统计页</Link>
+                    </li>
+                </ul>
+            </Nav>
+        </Wrapper>
     );
 }
+
+function Tags() {
+    return (
+        <div>标签页</div>
+    );
+}
+
+function Money() {
+    return (
+        <div>记账页</div>
+    );
+}
+
+function Statistics() {
+    return (
+        <div>统计页</div>
+    );
+}
+
+function Notfind() {
+    return (
+        <div>你访问的页面不存在</div>
+    );
+}
+
 
 export default App;
