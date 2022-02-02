377a917baed265b9654fa7d9e3b95ac79807a9b7
vinkim <928287706@qq.com>
2022-02-02 16:44:02 +0800

    安装配置 svg-sprite-loader 和添加 icon 图标

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,35 +1,55 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-
+require('icons/tag.svg')
+require('icons/chart.svg')
+require('icons/notePad.svg')
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   > ul {
     display: flex;
 
     > li {
       width: 33.3%;
       text-align: center;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding:4px 0;
+      .icon{
+        width: 24px;
+        height:24px;
+      }
     }
   }
 `;
 
-const Nav = ()=>{
-    return(
+const Nav = () => {
+    return (
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon" >
+                        <use xlinkHref="#tag" />
+                    </svg>
                     <Link to="/tags">标签页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#notePad" />
+                    </svg>
                     <Link to="/money">记账页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#chart" />
+                    </svg>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>
         </NavWrapper>
-    )
-}
+    );
+};
 export default Nav;
