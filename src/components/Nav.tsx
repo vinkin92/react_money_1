3cc864b5f4677014b1c3a66fff00f1e233eab010
vinkim <928287706@qq.com>
2022-02-02 19:23:07 +0800

    解决点击ICON无法跳转的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,11 +12,12 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.3%;
       text-align: center;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
       padding: 4px 0;
-
+      >a{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
       .icon {
         width: 24px;
         height: 24px;
@@ -30,16 +31,22 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
-                    <Icon name="tag"/>
-                    <Link to="/tags">标签页</Link>
+                    <Link to="/tags">
+                        <Icon name="tag"/>
+                        标签页
+                    </Link>
                 </li>
                 <li>
-                    <Icon name="notePad"/>
-                    <Link to="/money">记账页</Link>
+                    <Link to="/money">
+                        <Icon name="notePad"/>
+                        记账页
+                    </Link>
                 </li>
                 <li>
-                    <Icon name="chart"/>
-                    <Link to="/statistics">统计页</Link>
+                    <Link to="/statistics">
+                        <Icon name="chart"/>
+                        统计页
+                    </Link>
                 </li>
             </ul>
         </NavWrapper>
