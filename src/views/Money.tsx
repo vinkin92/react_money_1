9e898c13dc28ca749c2d3a7c15cc5301ad6a87a9
vinkim <928287706@qq.com>
2022-02-03 10:37:39 +0800

    记账页面基本结构

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,62 @@
 import Layout from '../components/Layout';
 import React from 'react';
+import styled from 'styled-components';
 
+const TagSection = styled.section`
+
+`
+const NotesSection = styled.section`
+
+`
+const CategorySection = styled.section`
+
+`
+const NumberPadSection = styled.section`
+
+`
 function Money() {
     return (
         <Layout>
-            <div>记账页</div>
+            <TagSection className="tags">
+                <ol>
+                    <li>衣</li>
+                    <li>食</li>
+                    <li>住</li>
+                    <li>行</li>
+                </ol>
+                <button>新增标签</button>
+            </TagSection>
+            <NotesSection>
+                <label>
+                    <span>备注</span>
+                    <input type="text"/>
+                </label>
+            </NotesSection>
+            <CategorySection>
+                <ul>
+                    <li>支出</li>
+                    <li>收入</li>
+                </ul>
+            </CategorySection>
+            <NumberPadSection>
+                <div>100</div>
+                <div>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>删除</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>清除</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>OK</button>
+                    <button>0</button>
+                    <button>.</button>
+                </div>
+            </NumberPadSection>
         </Layout>
     );
 }
