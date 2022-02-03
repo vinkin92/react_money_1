7e1978d861e7281b1593ffda7131b6da22249d74
vinkim <928287706@qq.com>
2022-02-03 16:27:14 +0800

    完成记账页面中的UI，是其整体下移

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,7 +5,11 @@ import styled from 'styled-components';
 const TagSection = styled.section`
   background-color: #FFF;
   padding: 12px 16px;
-
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items:flex-start;
   > ol {
     margin: 0 -12px;
 
@@ -152,9 +156,14 @@ const NumberPadSection = styled.section`
   }
 `;
 
+const MyLayout = styled(Layout)`
+  border:1px solid red;
+  display:flex;
+  flex-direction: column;
+`
 function Money() {
     return (
-        <Layout>
+        <MyLayout>
             <TagSection className="tags">
                 <ol>
                     <li>衣</li>
@@ -195,7 +204,7 @@ function Money() {
                     <button>.</button>
                 </div>
             </NumberPadSection>
-        </Layout>
+        </MyLayout>
     );
 }
 
