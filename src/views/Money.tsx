3ec9ec017c3f801286a6c894d5ac467a6d7f87ee
vinkim <928287706@qq.com>
2022-02-03 15:14:22 +0800

    完成记账页面中数字面板UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,45 +4,52 @@ import styled from 'styled-components';
 
 const TagSection = styled.section`
   background-color: #FFF;
-  padding:12px 16px;
-  >ol{
-    margin:0 -12px;
-    >li{
+  padding: 12px 16px;
+
+  > ol {
+    margin: 0 -12px;
+
+    > li {
       background-color: #D9D9D9;
-      border-radius:18px;
+      border-radius: 18px;
       display: inline-block;
       padding: 3px 18px;
-      font-size:14px;
-      margin:8px 12px;
+      font-size: 14px;
+      margin: 8px 12px;
     }
   }
-  >button{
+
+  > button {
     font-family: inherit;
     background: none;
-    border:none;
-    padding:2px 4px;
-    border-bottom:1px solid #333;
-    color:#333;
-    margin-top:8px;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #333;
+    margin-top: 8px;
   }
 `;
 const NotesSection = styled.section`
   background: #f5f5f5;
-  padding:0 16px;
-  font-size:14px;
-  >label{
+  padding: 0 16px;
+  font-size: 14px;
+
+  > label {
     display: flex;
     align-items: center;
-    >span{
+
+    > span {
       margin-right: 16px;
       white-space: nowrap;
     }
-    >input{
+
+    > input {
       border-style: none;
       background-color: #f5f5f5;
-      width:100%;
+      width: 100%;
       height: 72px;
-      &:focus{
+
+      &:focus {
         outline: none;
       }
     }
@@ -50,16 +57,19 @@ const NotesSection = styled.section`
 `;
 const CategorySection = styled.section`
   background-color: #c4c4c4;
-  font-size:24px;
-  > ul{
+  font-size: 24px;
+
+  > ul {
     display: flex;
-    >li{
-      width:50%;
+
+    > li {
+      width: 50%;
       text-align: center;
-      padding:16px 0;
+      padding: 16px 0;
       position: relative;
-      &.selected::after{
-        content:'';
+
+      &.selected::after {
+        content: '';
         display: block;
         height: 3px;
         background-color: #333;
@@ -72,7 +82,74 @@ const CategorySection = styled.section`
   }
 `;
 const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
 
+  > .output {
+    background-color: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+    inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25);
+  }
+
+  > .pad {
+    > button {
+      float: left;
+      width: 25%;
+      height: 64px;
+      font-size: 18px;
+      border: none;
+
+      &.ok {
+        height: 128px;
+        float: right;
+      }
+
+      &.zero {
+        width: 50%;
+      }
+
+      &:nth-child(1) {
+        background-color: #f2f2f2;
+      }
+
+      &:nth-child(2),
+      &:nth-child(5) {
+        background-color: #E0E0E0;
+      }
+
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background-color: #D3D3D3;
+      }
+
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background-color: #C1C1C1;
+      }
+
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background-color: #B8B8B8;
+      }
+
+      &:nth-child(12) {
+        background-color: #9A9A9A;
+
+      }
+
+      &:nth-child(14) {
+        background-color: #A9A9A9;
+      }
+
+    }
+  }
 `;
 
 function Money() {
@@ -100,8 +177,8 @@ function Money() {
                 </ul>
             </CategorySection>
             <NumberPadSection>
-                <div>100</div>
-                <div>
+                <div className="output">100</div>
+                <div className="pad clearfix">
                     <button>1</button>
                     <button>2</button>
                     <button>3</button>
@@ -113,8 +190,8 @@ function Money() {
                     <button>7</button>
                     <button>8</button>
                     <button>9</button>
-                    <button>OK</button>
-                    <button>0</button>
+                    <button className="ok">OK</button>
+                    <button className="zero">0</button>
                     <button>.</button>
                 </div>
             </NumberPadSection>
