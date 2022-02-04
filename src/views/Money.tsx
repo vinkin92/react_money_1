c1bdff4a9b0e648e2878f00a8d8bbcb0fcf84e0d
vinkim <928287706@qq.com>
2022-02-05 01:29:17 +0800

    合并money 组件中的 onChange 函数

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -17,6 +17,10 @@ function Money() {
         category:'-' as ('-' | '+'),
         amount:0
     })
+    type Selected = typeof selected;  // Selected 的类型的值为 selected
+    const onChange = (obj:Partial<Selected>)=>{  // Partial 表示支持部分类型，即可以忽略没有传的值，如果没有使用Partial，则必须传递所有的属性值
+        setSelected({...selected,...obj})
+    }
     return (
         <MyLayout>
             {selected.tags}
@@ -26,10 +30,10 @@ function Money() {
             {selected.category}
             <hr />
             {selected.amount}
-            <TagSection selected={selected.tags} onChange={(tags)=>setSelected({...selected,tags:tags})}/>
-            <NotesSection value={selected.note} onChange={(note)=>setSelected({...selected,note:note})}/>
-            <CategorySection value={selected.category} onChange={(category)=>setSelected({...selected,category:category})}/>
-            <NumberPadSection value={selected.amount} onChange={(value)=> setSelected({...selected,amount: value})}/>
+            <TagSection selected={selected.tags} onChange={tags=> onChange({tags})}/>
+            <NotesSection value={selected.note} onChange={note=> onChange({note})}/>
+            <CategorySection value={selected.category} onChange={category=> onChange({category})}/>
+            <NumberPadSection value={selected.amount} onChange={amount=> onChange({amount})}/>
         </MyLayout>
     );
 }
