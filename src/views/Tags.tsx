75a10df3df61b5212350f7eca902c48b06bcebc4
vinkim <928287706@qq.com>
2022-02-09 20:44:15 +0800

    Tags List 样式

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,10 +1,49 @@
 import Layout from '../components/Layout';
-import React from 'react';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
 
+const TagList = styled.ol`
+  font-size:16px;
+  background-color: white;
+  >li{
+    border-bottom: 1px solid #d5d5d9;
+    padding:12px 0;
+    margin:0 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+const Button = styled.button`
+  font-size:18px;
+  border:none;
+  padding: 8px 12px;
+  border-radius: 4px;
+  background-color: #767676;
+  color: white;
+`
+const Center = styled.div`
+display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 16px;
+`
 function Tags() {
+    const [tags,setTags] = useState<string[]>(['衣','食','住','行']);
     return (
         <Layout>
-            <div>标签页</div>
+            <TagList>
+            {
+                tags.map(tag => <li key={tag}>
+                    <span className="oneLine">{tag}</span>
+                    <Icon name="right" />
+                </li>)
+            }
+            </TagList>
+            <Center>
+            <Button>新增标签</Button>
+            </Center>
         </Layout>
     );
 }
