a1c3e5033f9cb6f84365a66287bc47fbb56a9079
vinkim <928287706@qq.com>
2022-02-10 19:57:48 +0800

    创建 TagEdit 路由并完成跳转

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Money from './views/Money';
 import Statistics from './views/Statistics';
 import Tags from './views/Tags';
 import Notfind from './views/Notfind';
+import {TagEdit} from './views/TagEdit';
 
 function App() {
     return (
         <Routes>
             <Route index element={<Money/>}/>
             <Route path="tags" element={<Tags/>}/>
+            <Route path="tags/:tag" element={<TagEdit/>}/>
             <Route path="money" element={<Money/>}/>
             <Route path="statistics" element={<Statistics/>}/>
             <Route path="*" element={<Notfind/>}/>
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -2,49 +2,57 @@ import Layout from '../components/Layout';
 import React, {useState} from 'react';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import {NavLink, Link} from 'react-router-dom';
 
 const TagList = styled.ol`
-  font-size:16px;
+  font-size: 16px;
   background-color: white;
-  >li{
+
+  > li {
     border-bottom: 1px solid #d5d5d9;
-    padding:12px 0;
-    margin:0 16px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    margin: 0 16px;
+    >a{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 12px 0;
+    }
   }
-`
+`;
 const Button = styled.button`
-  font-size:18px;
-  border:none;
+  font-size: 18px;
+  border: none;
   padding: 8px 12px;
   border-radius: 4px;
   background-color: #767676;
   color: white;
-`
+`;
 const Center = styled.div`
-display: flex;
+  display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 16px;
-`
+`;
+
 function Tags() {
-    const [tags,setTags] = useState<string[]>(['衣','食','住','行']);
+    const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
     return (
         <Layout>
             <TagList>
-            {
-                tags.map(tag => <li key={tag}>
-                    <span className="oneLine">{tag}</span>
-                    <Icon name="right" />
-                </li>)
-            }
+                {
+                    tags.map(tag => <li key={tag}>
+                        <Link to={'/tags/'+tag}>
+                            <span className="oneLine">{tag}</span>
+                            <Icon name="right"/>
+                        </Link>
+                    </li>)
+                }
             </TagList>
             <Center>
-            <Button>新增标签</Button>
+                <Button>新增标签</Button>
             </Center>
         </Layout>
     );
 }
+
 export default Tags;
