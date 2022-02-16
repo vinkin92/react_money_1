26d437397f12493dacbbe39facdf74ebe5a76f35
vinkim <928287706@qq.com>
2022-02-17 07:37:34 +0800

    檢查輸入内容

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -24,8 +24,17 @@ type newRecordItem = Omit<RecordItem, 'createdAt'>
 export const useRecords = () =>{
     const [records,setRecords] = useState<RecordItem[]>([])
     const addRecord = (newRecord:newRecordItem) =>{
+        if(newRecord.amount <= 0 ){
+            alert('請輸入金額')
+            return false
+        }
+        if(newRecord.tagIds.length === 0){
+            alert('請選擇標簽')
+            return false
+        }
         const record = {...newRecord,createdAt:(new Date()).toISOString()}
         setRecords([...records,record])
+        return true
     }
     useEffect(()=>{
         setRecords(JSON.parse(window.localStorage.getItem('records') || '[]'))
